dad0171dc4157003824bb3e1e718804f1be73e9d
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-12-19 18:28:36 +0530

    user route.js

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", isAuthenticated, login);
+router.post("/login", login);
 router.get("/logout", logout);
 router.get("/me", getMyProfile);
 
