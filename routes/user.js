e5555b5e83705d280937eae23cdf1941a1824581
Abhishek kumawat <abhikumardambiwal@gmail.com>
2024-12-20 18:44:17 +0530

    auth fix

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
-router.get("/me", getMyProfile);
+router.get("/me", isAuthenticated, getMyProfile);
 
 export default router;
